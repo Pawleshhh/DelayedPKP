8fddbc378b3f1d51349f2db97c6c7bf967c49014
Pawleshhh <mateusz46668@gmail.com>
2019-04-27 11:07:34 +0200

    Added new exception classes and changed the exceptions the page reader throws

---
+++ DelayedPKP/Model/Exceptions/DataNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelayedPKP.Model
+{
+    /// <summary>
+    /// Exception that occurs when the data has not been found.
+    /// </summary>
+    public class DataNotFoundException : Exception
+    {
+
+        public DataNotFoundException() :base("Data was not found") { }
+
+        public DataNotFoundException(Exception inner) : base("Data was not found", inner) { }
+
+        public DataNotFoundException(string message) : base(message) { }
+
+        public DataNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
---
+++ DelayedPKP/Model/Exceptions/WrongDataProvidedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelayedPKP.Model
+{
+
+    /// <summary>
+    /// Exception that occurs when data that has been provided is not correct.
+    /// </summary>
+    public class WrongDataProvidedException : Exception
+    {
+
+        public WrongDataProvidedException() : base() { }
+
+        public WrongDataProvidedException(string message) : base(message) { }
+
+        public WrongDataProvidedException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
